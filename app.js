de74bd4c3513813f93de315e36e40d8072e66534
Tommaso Girotto <tommaso.girotto91@gmail.com>
2019-11-10 21:45:36 +0300

    ready

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
   }),
   secret: "gopamoja!",
   resave: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, secure: false},
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000},
   saveUninitialized: false
 }));
 
